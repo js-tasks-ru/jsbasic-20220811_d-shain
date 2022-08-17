75c86d77e9ba1961db24609835b16f1900f2edd7
Dmitriy Shain <d_shain@mail.ru>
2022-08-17 11:42:25 +0300

    a name validation function has been created

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,8 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if ((name != null) && (name.indexOf(' ') == -1) && (name.length > 3)) return true;
+    else return false; // ваш код...
 }
 
 function sayHello() {
