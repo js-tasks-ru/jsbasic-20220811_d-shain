9e7a124ec81a070633f84ab29b33716ebac8208d
Dmitriy Shain <d_shain@mail.ru>
2022-08-31 17:40:18 +0300

    min/max is ready

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ');
+      let array = [];
+      arr.forEach((element) => {
+       // alert(Number(element))
+        if (isNaN(Number(element)));
+          else array.push(element);
+      });
+      let result = {
+        min: Math.min(...array),
+        max: Math.max(...array)
+      };
+      return result;
 }
