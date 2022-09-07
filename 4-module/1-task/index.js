a22cc562e1b9f54360a5952700542e45c1a3e11d
Dmitriy Shain <d_shain@mail.ru>
2022-09-07 08:56:51 +0300

    4-modele 1-task error

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
-}
+  let ul = document.createElement('ul');
+  ul.className = 'list';
+  document.body.append(ul);
+  let list = document.querySelector('.list');
+  for (let item of friends) {
+    let li = document.createElement('LI');
+    li.innerHTML = item.firstName + ' ' + item.lastName;
+    list.append(li);
+  }
+  return list;
+  }// ваш код...
