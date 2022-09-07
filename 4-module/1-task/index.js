2846dd73cddc9b1f7baf0ab7694669f36737dd23
Dmitriy Shain <d_shain@mail.ru>
2022-09-07 10:24:13 +0300

    4-modele 1-task is ready

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,12 +1,10 @@
 function makeFriendsList(friends) {
   let ul = document.createElement('ul');
   ul.className = 'list';
-  document.body.append(ul);
-  let list = document.querySelector('.list');
   for (let item of friends) {
     let li = document.createElement('LI');
     li.innerHTML = item.firstName + ' ' + item.lastName;
-    list.append(li);
+    ul.append(li);
   }
-  return list;
+  return ul;
   }// ваш код...
