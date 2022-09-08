169e21c39b9423877077df04e8dea4d21f41bb43
Dmitriy Shain <d_shain@mail.ru>
2022-09-08 17:23:55 +0300

    4-module 3-task HELP 2

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,10 +1,10 @@
 function highlight(table) {
   for (let tr of table.tBodies) {
-    if (tr.lastElementChild.data-available == null) tr.hidden = true;
-      else if (tr.lastElementChild.data-available == true) tr.classList.add('available');
+    if (tr.lastElementChild.dataset.available == null) tr.hidden = true;
+      else if (tr.lastElementChild.dataset.available == true) tr.classList.add('available');
         else tr.classList.add('unavailablee');
-    if (tr.children[2].innerHTML == 'm') tr.classList.add('male');
-      else if (tr.children[2].innerHTML == 'f') tr.classList.add('female');
-    if (tr.children[1].innerHTML < 18) tr.style = "text-decoration: line-through";
+    if (tr.children[2] == 'm') tr.classList.add('male');
+      else if (tr.children[2] == 'f') tr.classList.add('female');
+    if (tr.children[1] < 18) tr.style = "text-decoration: line-through";
   }    
 }
