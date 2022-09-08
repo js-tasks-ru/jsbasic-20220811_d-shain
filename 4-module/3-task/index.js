8b777153a9ad45054964ab63881acd194f916b7f
Dmitriy Shain <d_shain@mail.ru>
2022-09-08 16:33:27 +0300

    4-module 3-task HELP

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,10 @@
 function highlight(table) {
-  // ваш код...
+  for (let tr of table.tBodies) {
+    if (tr.lastElementChild.data-available == null) tr.hidden = true;
+      else if (tr.lastElementChild.data-available == true) tr.classList.add('available');
+        else tr.classList.add('unavailablee');
+    if (tr.children[2].innerHTML == 'm') tr.classList.add('male');
+      else if (tr.children[2].innerHTML == 'f') tr.classList.add('female');
+    if (tr.children[1].innerHTML < 18) tr.style = "text-decoration: line-through";
+  }    
 }
