f396153bc3c12875779559196f7bcd7fd887ac36
Dmitriy Shain <d_shain@mail.ru>
2022-09-12 09:07:54 +0300

    4-module 3-task is ready

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -2,7 +2,7 @@ function highlight(table) {
   for (let tr of table.children[1].children) {
     if (tr.lastElementChild.dataset.available == null) tr.hidden = true;
     if (tr.lastElementChild.dataset.available == 'true') tr.classList.add('available');
-    if (tr.lastElementChild.dataset.available == 'false') tr.classList.add('unavailablee');
+    if (tr.lastElementChild.dataset.available == 'false') tr.classList.add('unavailable');
     if (tr.children[2].innerHTML == 'm') tr.classList.add('male');
       else if (tr.children[2].innerHTML == 'f') tr.classList.add('female');
     if (Number(tr.children[1].innerHTML) < 18) {
